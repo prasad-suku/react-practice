e1a01607a3b49a280927403debfc8979acfb30a1
prasad-suku <142576212+prasad-suku@users.noreply.github.com>
2024-07-14 04:21:12 -0400

    Add files via upload

---
+++ anothercom.jsx
@@ -0,0 +1,14 @@
+//  import React from 'react'
+ 
+ function Anothercom(prop){
+    return (
+        <>
+        <h2>my name is {prop.name}</h2>
+        <h2> my age is {prop.age}</h2>
+        <h2> and i am from {prop.place} </h2>
+       
+        </>
+    )
+ }
+
+ export default Anothercom;
