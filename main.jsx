9ecca1bd2f0b92776c8fcb86d60ffc8bf2fadbc5
prasad-suku <142576212+prasad-suku@users.noreply.github.com>
2024-07-16 03:01:45 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,20 @@
+
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+import './index.css'
+// import Lists from './List'
+import { StrictMode } from 'react'
+import Listkey1 from './Listkey1'
+// import { peopledatas } from './peopleDatas'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+<StrictMode>  
+
+<Listkey1 />
+   
+</StrictMode>
+  
+)
