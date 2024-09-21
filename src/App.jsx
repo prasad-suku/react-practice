4aa97745f9c61f841ff4a1ede760c304db192659
prasad-suku <142576212+prasad-suku@users.noreply.github.com>
2024-09-21 05:52:00 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import React from "react";
-import Parent from "./component/Parent";
+import React, { useState } from "react";
+import "./index.css";
+import { FaStar } from "react-icons/fa";
 
-import Usereducerhook from "./Usereducerhook";
+const App = ({ no_ofstars = 7 }) => {
+  const [rating, setrating] = useState(0);
+  const [hover, sethover] = useState(0);
+  // hadleclick function
 
-const App = () => {
+  const handleclick = (ind) => {
+    setrating(ind);
+    console.log(ind);
+  };
+
+  // handlemousehover
+  const handlemousehover = (ind) => {
+    sethover(ind);
+    console.log(ind);
+  };
+
+  // handlemouseleave
+  const handlemouseleave = () => {
+    sethover(rating);
+  };
   return (
     <div>
-      App
-      <Parent></Parent>
-      {/* <Usereducerhook /> */}
+      <div className="star-rating">
+        {[...Array(no_ofstars)].map((_, ind) => {
+          ind += 1;
+          return (
+            <FaStar
+              size={70}
+              className={ind <= (rating || hover) ? "active" : "inactive"}
+              key={ind}
+              onClick={() => {
+                handleclick(ind);
+              }}
+              onMouseEnter={() => handlemousehover(ind)}
+              onMouseLeave={() => handlemouseleave()}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
